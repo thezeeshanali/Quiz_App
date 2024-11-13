13a51b9eebc0363e223e24723dc1662dca44e129
Zeeshan <thealizeeshan@gmail.com>
2024-11-13 14:55:11 +0500

    migrations created

--- database/migrations/2024_11_13_095223_create_answers_table.php
+++ database/migrations/2024_11_13_095223_create_answers_table.php
@@ -11,9 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('answers', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->foreignId('question_id')->constrained()->onDelete('cascade');
+            $table->text('text');
+            $table->boolean('is_correct')->default(false);
             $table->timestamps();
         });
     }
@@ -23,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('answers');
     }
 };
